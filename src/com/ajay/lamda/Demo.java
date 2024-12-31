6d6432bd26f513ff841d712ce21d6cba00f7d121
Ajay-Babare <ajaybabare97@gmail.com>
2024-12-31 18:10:31 +0530

    Lamda Expression

---
+++ src/com/ajay/lamda/Demo.java
@@ -0,0 +1,11 @@
+package com.ajay.lamda;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		Test t =  () -> System.out.println("Test Interface Lamda Implementation");
+		t.display();
+		
+	}
+}
